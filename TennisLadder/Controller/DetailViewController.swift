9ebf365ee264356b55312f5101da728b5374ba94
Z Tai <asterixandisaac@gmail.com>
2018-12-20 12:01:41 -0700

    finished basic layout for detailViewController

--- TennisLadder/Controller/DetailViewController.swift
+++ TennisLadder/Controller/DetailViewController.swift
@@ -10,6 +10,9 @@ import Foundation
 import UIKit
 
 class DetailViewController : UIViewController {
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
